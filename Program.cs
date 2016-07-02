f411ff4bbb6a3ee7999e618cc8f9612d57198eba
Luke Carrier <luke@carrier.im>
2016-07-02 14:00:00 +0100

    Empty .NET project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChromiumPlating
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
